92aec9cc38708542ff53290d7f183a6ee7e934ce
AriaMortazavi <ariamortazavi@ymail.com>
2021-02-23 09:11:55 -0800

    changed text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,11 +29,11 @@ class About extends Component {
             <span>Hello, I'm</span>
             <span>Aria Mortazavi</span>
             <span>
-              I am a Front-End Software Developer and UX/UI Designer who is passionate about
+              I am a Front-End Software Developer and UX/UI Designer. I am assionate about
               developing scalable, dynamic, cutting-edge web and mobile
               apps. Using modern Software Architecture, I
-              can work in a team to deliver high
-              quality result that exceeds
+              can work in alone or in teams to deliver high
+              quality results that exceed
               expectation.
             </span>
             <span>Find me on</span>
