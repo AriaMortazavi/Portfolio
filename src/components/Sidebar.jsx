bbc931742e37235d124d9d030de4da61518d9a75
AriaMortazavi <ariamortazavi@ymail.com>
2021-03-01 11:19:02 -0800

    fixed vercel

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,7 +37,7 @@ class Sidebar extends Component {
               className="portfolioPicture"
               style={this.portfolioPictureStyle()}
             />
-            <span className="p-span">📞 (778) 683 - 4414</span>
+            <span className="p-span"><span role="img">📞</span> (778) 683 - 4414</span>
             <br></br> <br></br>
             <span className="s-span">Front-End &#38; UX/UI Designer</span>
             <div className="p-div">
