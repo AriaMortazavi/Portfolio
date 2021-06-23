8d874e8abe09d44f2f8bccae4f75c30b1ee2efd3
AriaMortazavi <55810685+AriaMortazavi@users.noreply.github.com>
2021-06-23 10:19:10 -0700

    fre

--- src/components/PhotoshopPoster.jsx
+++ src/components/PhotoshopPoster.jsx
@@ -73,6 +73,7 @@ class PhotoshopPoster extends Component {
               >
                 Visit
               </a>
+              <br></br>
               <a
                 className="hire-span"
                 target="_blank" rel="noopener noreferrer" href="https://drive.google.com/uc?export=download&id=1Ceh2HHW5vlGgZkpYckleO-8hVh0WYNUY"
